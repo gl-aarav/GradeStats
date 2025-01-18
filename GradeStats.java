ce9602f99c448bd4803fb75bd2f20951586dd8b6
gl-aarav <gl.aarav@gmail.com>
2025-01-17 20:12:23 -0800

    update

--- GradeStats.java
+++ GradeStats.java
@@ -1,62 +1,95 @@
-/*
- * Aarav Goyal
- * 1/17/2025
- * GradeStats.java
- */ 
 import java.util.Scanner;
-public class GradeStats
+
+public class GradeStats 
 {
-	int[] grades;
-	public GradeStats()
-	{
-	}
-	
-	public static void main (String[] args)
+	int[] grades = new int[500];
+	public static void main(String[] args) 
 	{
 		GradeStats gs = new GradeStats();
 		gs.calculateIt();
 	}
-	
+
 	public void calculateIt()
-	{
-		int numScores, min, max, avg, median = 0;
-		getInput();
-		bubbleSort();
-		printData();
-	}
-	
-	public void getInput()
-	{
-		Scanner in = new Scanner (System.in);
-		for (int x = 0; !x.equalsIgnoreCase("quit"); x++)
+	{	
+		int numScores = 0;
+
+		Scanner in = new Scanner(System.in);
+		boolean next = true;
+		System.out.println("\n\n\n");
+		while (next) 
 		{
-			System.out.print("\n\n\nType in the score. Type “Quit” to end the program --> ");
-			String temp  = in.nextLine;
-			temp.parseInt();
+			System.out.print("\nEnter a score (or type 'Quit' to stop): ");
+			String input = in.next();
+
+			if (input.equalsIgnoreCase("Quit")) 
+			{
+				next = false;
+			}
+			else
+			{
+				int score = Integer.parseInt(input);
+				if (numScores < grades.length) 
+				{
+					grades[numScores] = score;
+					numScores++;
+				} 
+				else 
+				{
+					System.out.println("Maximum score limit reached.");
+					next = false;
+				}
+			}
+
+		}
+
+		if (numScores == 0) 
+		{
+			System.out.println("No scores entered.");
+		}
+		else
+		{
+			sort(numScores);
+			printData(numScores);
 		}
 	}
-	
-	public static void bubbleSort() 
+
+	public void printData(int numScores)
 	{
-		int n = grades.length;
-		int temp = 0;
-		for (int i = 0; i < n; i++) 
+		System.out.println("\nScores:");
+		for (int i = 0; i < numScores; i++) 
 		{
-			for (int j = 1; j < (n - i); j++) 
+			System.out.println("Student " + (i + 1) + ": " + grades[i]);
+		}
+
+		int min = grades[0];
+		int max = grades[numScores - 1];
+		int sum = 0;
+		int median = grades[numScores/2];
+		for (int i = 0; i < numScores; i++) 
+		{
+			sum += grades[i];
+		}
+		double avg = sum / (double) numScores;
+		System.out.println("\n\nMin score: " + min);
+		System.out.println("Max score: " + max);
+		System.out.println("Median score: " + median);
+		System.out.println("Average score: " + avg + "\n\n\n");	
+	}
+
+	public void sort(int numScores)
+	{
+		for (int i = 0; i < numScores - 1; i++) 
+		{
+			for (int j = 0; j < numScores - 1 - i; j++)
 			{
-				if (grades[j - 1] > grades[j]) 
+				if (grades[j] > grades[j + 1]) 
 				{
-					temp = grades[j - 1];
-					grades[j - 1] = arr[j];
-					grades[j] = temp;
+					int temp = grades[j];
+					grades[j] = grades[j + 1];
+					grades[j + 1] = temp;
 				}
 			}
 		}
 	}
-	
-	public static void printData()
-	{
-		for (int i = 0; i < grades.length; i++)
-			System.out.println("Student " + (i+1) + "'s score: " + grades[i]);
-	}
 }
+
